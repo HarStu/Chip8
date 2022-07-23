ced4096d353e20ac2dcc8b0c5325dcf43b0c15f6
h <harrisonmstuart@gmail.com>
2022-07-23 15:00:44 -0400

    Define Chip8 struct in chip8.h

--- chip8.h
+++ chip8.h
@@ -1,16 +1,26 @@
 #ifndef _CHIP8_H
 #define _CHIP8_H
 
-//loads data from file into mem
+// struct holding the full state of a Chip-8 system
+typedef struct Chip8 {
+    unsigned char v[16];       // registers
+    unsigned short pc;         // progam counter
+    unsigned short I;          // index register
+    unsigned short stack[16];  // stack
+    unsigned short sp;         // stack pointer
+    unsigned char mem[4096];   // memory
+} Chip8;
+
+// loads data from file into mem
 void loadData(unsigned char *mem, int memSize, const char *path);
 
-//outputs a dump of the data loaded into [mem]
+// outputs a dump of the data loaded into [mem]
 void outputMemDump();
 
-//loads mem full of empty data
+// loads mem full of empty data
 void loadEmptyMem();
 
-//prints the value of each register, sp, and pc
+// prints the value of each register, sp, and pc
 void statusDump();
 
 #endif // _CHIP8_H
