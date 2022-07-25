03ae34b09eba5904a685732cf87d9db123d2643f
h <harrisonmstuart@gmail.com>
2022-07-25 18:18:24 -0400

    Update opcodes 1NNN, 6XNN, and 7XNN
    All these opcodes are now more readable (using the preprocessor
    directive shorthands) and (should) adhere to the behavioral standards in
    Matthew Mikolay's Chip-8 Instruction Set

--- cpu.c
+++ cpu.c
@@ -40,7 +40,7 @@ void cycle(Chip8 *c8, FILE *out) {
                 // 0x00E0
                 // clear screen
                 for (int x = 0; x < 64; x++) {
-                    for (int y = 0; < 32; y++) {
+                    for (int y = 0; y < 32; y++) {
                         c8->screen[x][y] = 0;
                     }
                 }
@@ -53,23 +53,23 @@ void cycle(Chip8 *c8, FILE *out) {
             }
             break;
 
-        case 0x1000: //old
+        case 0x1000:
             //0x1NNN
             //jump to address NNN in 0x1NNN
-            c8->pc = (opcode & 0x0FFF);	
+            c8->pc = NNN;	
             break;
 
-        case 0x2000: //old
+        case 0x2000:
             //0x2NNN
             //execute subroutine at NNN
             c8->stack[c8->sp] = c8->pc;
             c8->sp++;
-            c8->pc = (opcode & 0x0FFF);
+            c8->pc = NNN;
             break;
 
         case 0x3000: //old
             //0x3XNN
-            //skip the following instruction if VX == NN
+            //skip the following instruction if vX == NN
             reg = (opcode & 0x0F00) >> 8;
             if (c8->v[reg] == (opcode & 0x00FF)) {
                 c8->pc += 4; //values matched, next instruction is skipped
@@ -81,7 +81,7 @@ void cycle(Chip8 *c8, FILE *out) {
 
         case 0x4000: //old
             //0x4XNN
-            //skip the following instruction if VX != NN
+            //skip the following instruction if vX != NN
             reg = (opcode & 0x0F00) >> 8;
             if (c8->v[reg] != (opcode & 0x00FF)) {
                 c8->pc += 4;
@@ -93,7 +93,7 @@ void cycle(Chip8 *c8, FILE *out) {
 
         case 0x5000: //old
             //5XY0
-            //skip the following instruction if VX is equal to VY
+            //skip the following instruction if vX is equal to vY
             if(c8->v[(opcode & 0x0F00) >> 8] == c8->v[(opcode & 0x00F0) >> 4]) {
                 c8->pc += 4;
             }
@@ -102,49 +102,49 @@ void cycle(Chip8 *c8, FILE *out) {
             }
             break;
 
-        case 0x6000: //old
+        case 0x6000:
             //6XNN
-            //Store the value of NN in register VX
-            c8->v[((opcode & 0x0F00) >> 8)] = c8->mem[c8->pc + 1];
+            //Store the value of NN in register vX
+            c8->v[X] = NN;
             c8->pc += 2;
             break;
 
-        case 0x7000: //old
+        case 0x7000:
             //7XNN
-            //Add the value of NN to register VX
-            c8->v[((opcode & 0x0F00) >> 8)] = (c8->v[((opcode & 0x0F00) >> 8)] +  c8->mem[c8->pc + 1]) % 256;
+            //Add the value of NN to register vX
+            c8->v[X] = c8->v[X] + NN;
             c8->pc += 2;
             break;
 
         case 0x8000: //old
             //8XY0
-            //store the value of VY in VX
+            //store the value of vY in vX
             if ((opcode & 0x000F) == 0x0000) {
                 c8->v[((opcode & 0x0F00) >> 8)] = c8->v[((opcode & 0x00F0) >> 4)];
                 c8->pc += 2;
             }
             //8XY1
-            //set the value of VX to VX OR VY
+            //set the value of vX to vX OR vY
             else if ((opcode & 0x000F) == 0x0001) {
                 c8->v[((opcode & 0x0F00) >> 8)] = (c8->v[((opcode & 0x0F00) >> 8)] | c8->v[((opcode & 0x00F0) >> 4)]);
                 c8->pc += 2;
             }
             //8XY2
-            //set the value of VX to VX AND VY
+            //set the value of vX to vX AND vY
             else if ((opcode & 0x000F) == 0x0002) {
                 c8->v[((opcode & 0x0F00) >> 8)] = (c8->v[((opcode & 0x0F00) >> 8)] & c8->v[((opcode & 0x00F0) >> 4)]);
                 c8->pc += 2;
             }
             //8XY3
-            //set the value of VX to VX XOR VY
+            //set the value of vX to vX XOR vY
             else if ((opcode & 0x000F) == 0x0003) {
                 c8->v[((opcode & 0x0F00) >> 8)] = (c8->v[((opcode & 0x0F00) >> 8)] ^ c8->v[((opcode & 0x00F0) >> 4)]);
                 c8->pc += 2;
             }
             //8XY4
-            //Add the value of VY to VX
-            //If a carry occurs, set VF to 01
-            //If a carry does not occur, set VF to 00
+            //Add the value of vY to vX
+            //If a carry occurs, set vF to 01
+            //If a carry does not occur, set vF to 00
             else if ((opcode & 0x000F) == 0x0004) {
                 if (c8->v[((opcode & 0x0F00) >> 8)] + c8->v[((opcode & 0x00F0) >> 4)] > 255) {
                     //carry occurs
@@ -158,9 +158,9 @@ void cycle(Chip8 *c8, FILE *out) {
                 c8->pc += 2;
             }
             //8XY5
-            //Subtract the value of VY from VX
-            //Set VF to 00 if a borrow occurs
-            //Set VF to 01 if a borrow does not occur
+            //Subtract the value of vY from vX
+            //Set vF to 00 if a borrow occurs
+            //Set vF to 01 if a borrow does not occur
             //TODO - Somewhat unclear on the concept of a borrow in Binary math.
             //Some revision may be needed
             else if ((opcode & 0x000F) == 0x0005) {
