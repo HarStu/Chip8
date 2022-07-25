2f2ce14d51c9f627782a52a21980aacc59bb836a
h <harrisonmstuart@gmail.com>
2022-07-25 08:30:36 -0400

    Fix cycle() prototype in cpu.h

--- cpu.h
+++ cpu.h
@@ -1,8 +1,10 @@
 #ifndef _CPU_H
 #define _CPU_H
 
+#include "chip8.h"
+
 // fetch, decode, and execute a chip-8 opcode
 // increment pc by 2 bytes after
-void cycle();
+void cycle(Chip8 *c8);
 
 #endif // _CPU_H
