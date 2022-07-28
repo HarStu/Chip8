42f243898d447b10902dcd4e657db36f7d418b9c
h <harrisonmstuart@gmail.com>
2022-07-28 10:04:22 -0400

    Clean up cpu.c
    Updated all the old opcodes to use the preprocessor directives (much
    more readable now) and changed where the pc is typically incremented

--- cpu.c
+++ cpu.c
@@ -13,14 +13,17 @@
 
 void cycle(Chip8 *c8, FILE *out) {
     // fetch opcode
+    // increment pc by 2 bytes
     // decode opcode
     // execute opcode
-    // increment pc by 2 (16 bits)
 
     // all chip8 opcodes are 16-bit (2-byte) values, which we can represent as an unsigned short
     // here, the value mem[pc] is shifted 8 zeroes to the left, becoming the first 8 bits
     // then, a bitwise OR merges it with the data point next to it, which becomes the next 8 bits
     unsigned short opcode = c8->mem[c8->pc] << 8 | c8->mem[c8->pc+1];
+
+    // iterate pc here; next time an opcode is fetched, it'll be the one after the current opcode
+    c8->pc += 2;
 
     // declaring this variable here since it cannot be properly
     // used within the switch statement
@@ -30,7 +33,7 @@ void cycle(Chip8 *c8, FILE *out) {
     // the exact opcode executed is determined by other nibbles, 
     // which we've already defined as preprocessor directives
     switch(opcode & 0xF000) {
-        case 0x0000: //old
+        case 0x0000: 
             if (opcode == 0x00EE) {
                 //0x00EE
                 //return from a subroutine
@@ -40,25 +43,14 @@ void cycle(Chip8 *c8, FILE *out) {
             else if (opcode == 0x00E0) {
                 // 0x00E0
                 // clear screen
-                for (int x = 0; x < 64; x++) {
-                    for (int y = 0; y < 32; y++) {
-                        c8->screen[x][y] = 0x0;
-                    }
-                }
-                c8->pc += 2;
-            }
-            else {
-                //*TODO*execute machine languge instruction at 0x0NNN
-                //we're probably not going to implement this
-                //so just forget about it
-                c8->pc += 2;
+                clearScreen(c8);
             }
             break;
 
         case 0x1000:
             //0x1NNN
             //jump to address NNN in 0x1NNN
-            c8->pc = NNN;	
+            c8->pc = NNN;
             break;
 
         case 0x2000:
@@ -69,38 +61,27 @@ void cycle(Chip8 *c8, FILE *out) {
             c8->pc = NNN;
             break;
 
-        case 0x3000: //old
+        case 0x3000:
             //0x3XNN
             //skip the following instruction if vX == NN
-            reg = (opcode & 0x0F00) >> 8;
-            if (c8->v[reg] == (opcode & 0x00FF)) {
-                c8->pc += 4; //values matched, next instruction is skipped
-            }
-            else {
-                c8->pc += 2; //values didn't match, pc is incremented normally
+            if (c8->v[X] == NN) {
+                c8->pc += 2; // pc is iterated by another 2 bytes, skipping the next opcode
             }
             break;
 
-        case 0x4000: //old
+        case 0x4000:
             //0x4XNN
             //skip the following instruction if vX != NN
-            reg = (opcode & 0x0F00) >> 8;
-            if (c8->v[reg] != (opcode & 0x00FF)) {
-                c8->pc += 4;
-            }
-            else {
-                c8->pc += 2;
+            if (c8->v[X] != NN) {
+                c8->pc += 2; // pc is iterated by another 2 bytes, skipping the next opcode
             }
             break;
 
-        case 0x5000: //old
+        case 0x5000:
             //5XY0
             //skip the following instruction if vX is equal to vY
-            if(c8->v[(opcode & 0x0F00) >> 8] == c8->v[(opcode & 0x00F0) >> 4]) {
-                c8->pc += 4;
-            }
-            else {
-                c8->pc += 2;
+            if(c8->v[X] == c8->v[Y]) {
+                c8->pc += 2; // pc is iterated by another 2 bytes, skipping the next opcode
             }
             break;
 
@@ -108,47 +89,41 @@ void cycle(Chip8 *c8, FILE *out) {
             //6XNN
             //Store the value of NN in register vX
             c8->v[X] = NN;
-            c8->pc += 2;
             break;
 
         case 0x7000:
             //7XNN
             //Add the value of NN to register vX
             c8->v[X] = c8->v[X] + NN;
-            c8->pc += 2;
             break;
 
         case 0x8000: //old
             //8XY0
             //store the value of vY in vX
-            if ((opcode & 0x000F) == 0x0000) {
-                c8->v[((opcode & 0x0F00) >> 8)] = c8->v[((opcode & 0x00F0) >> 4)];
-                c8->pc += 2;
+            if (N == 0x0000) {
+                c8->v[X] = c8->v[Y];
             }
             //8XY1
             //set the value of vX to vX OR vY
-            else if ((opcode & 0x000F) == 0x0001) {
-                c8->v[((opcode & 0x0F00) >> 8)] = (c8->v[((opcode & 0x0F00) >> 8)] | c8->v[((opcode & 0x00F0) >> 4)]);
-                c8->pc += 2;
+            else if (N == 0x0001) {
+                c8->v[X] = (c8->v[X] | c8->v[Y]);
             }
             //8XY2
             //set the value of vX to vX AND vY
-            else if ((opcode & 0x000F) == 0x0002) {
-                c8->v[((opcode & 0x0F00) >> 8)] = (c8->v[((opcode & 0x0F00) >> 8)] & c8->v[((opcode & 0x00F0) >> 4)]);
-                c8->pc += 2;
+            else if (N == 0x0002) {
+                c8->v[X] = (c8->v[X] & c8->v[Y]);
             }
             //8XY3
             //set the value of vX to vX XOR vY
-            else if ((opcode & 0x000F) == 0x0003) {
-                c8->v[((opcode & 0x0F00) >> 8)] = (c8->v[((opcode & 0x0F00) >> 8)] ^ c8->v[((opcode & 0x00F0) >> 4)]);
-                c8->pc += 2;
+            else if (N == 0x0003) {
+                c8->v[X] = (c8->v[X] ^ c8->v[Y]);
             }
             //8XY4
             //Add the value of vY to vX
             //If a carry occurs, set vF to 01
             //If a carry does not occur, set vF to 00
-            else if ((opcode & 0x000F) == 0x0004) {
-                if (c8->v[((opcode & 0x0F00) >> 8)] + c8->v[((opcode & 0x00F0) >> 4)] > 255) {
+            else if (N == 0x0004) {
+                if (c8->v[X] + c8->v[Y] > 255) {
                     //carry occurs
                     c8->v[0xF] = 0x01;
                 }
@@ -156,28 +131,22 @@ void cycle(Chip8 *c8, FILE *out) {
                     //no carry occurs
                     c8->v[0xF] = 0x00;
                 }
-                c8->v[((opcode & 0x0F00) >> 8)] = (c8->v[((opcode & 0x0F00) >> 8)] +  c8->mem[c8->pc + 1]) % 256;
-                c8->pc += 2;
+                c8->v[X] = (c8->v[X] + c8->v[Y]) % 256;
             }
             //8XY5
             //Subtract the value of vY from vX
-            //Set vF to 00 if a borrow occurs
-            //Set vF to 01 if a borrow does not occur
-            //TODO - Somewhat unclear on the concept of a borrow in Binary math.
-            //Some revision may be needed
-            else if ((opcode & 0x000F) == 0x0005) {
-                if (c8->v[((opcode & 0x00F0) >> 4)] > c8->v[((opcode & 0x0F00) >> 8)]) {
+            //Set vF to 00 if a borrow occurs from beyond the highest bit
+            //Set vF to 01 if a borrow does not occur from beyond the highest bit
+            else if (N == 0x0005) {
+                if (c8->v[Y] > c8->v[X]) {
                     //borrow occurs
                     c8->v[0xF] = 0x00;
                 }
                 else {
                     c8->v[0xF] = 0x01;
                 }
-                c8->v[((opcode & 0x0F00) >> 8)] = c8->v[((opcode & 0x0F00) >> 8)] - c8->v[((opcode & 0x00F0) >> 4)];
-                c8->pc += 2;
-
+                c8->v[X] = c8->v[X] - c8->v[Y];
             }
-
             break;
 
         case 0x9000:
@@ -186,14 +155,13 @@ void cycle(Chip8 *c8, FILE *out) {
             // 0xANNN
             // Store the value of NNN in the index register
             c8->I = NNN;
-            c8->pc += 2;
             break;
 
         case 0xB000:
             break;
         case 0xC000:
             break;
-        case 0xD000:
+        case 0xD000: //TODO: NEED PROTECTIONS TO PREVENT DRAWING OUT OF BOUNDS!
             // DXYN
             // draw a spite N pixels tall (always 8 bit/1 byte wide),
             // from the memory location held in the index register I
@@ -236,8 +204,8 @@ void cycle(Chip8 *c8, FILE *out) {
                 }
             }
             fprintf(out,"*");
-            c8->pc += 2;
             break;
+
         case 0xE000:
             break;
         case 0xF000:
