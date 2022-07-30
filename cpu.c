ed24964d6fd27a0dffa3565bf0d3d71b4f56305b
h <harrisonmstuart@gmail.com>
2022-07-29 23:06:14 -0400

    Implement opcodes 8XY6 and 8XYE
    These ones were a headache, 8XY6 wouldn't pass the test for some reason-
    but implementing it would make 8XYE pass the test! Once I had them both
    implemented, they both passed flawlessly. Weird, but seems to work fine;
    probably just a quirk of the test in question

--- cpu.c
+++ cpu.c
@@ -146,13 +146,10 @@ void cycle(Chip8 *c8, FILE *out) {
             // 8XY6
             // Store the value of vY shifted one bit right in vX
             // Set vF to the least significant bit of vY prior to the shift
-            /*
-            CURRENTLY NOT PASSING TEST 
             else if (N == 0x0006) {
                 c8->v[0xF] = (c8->v[Y] & 0x1); // store least significant bit in vF
                 c8->v[X] = (c8->v[Y] >> 1);
             }
-            */
             // 8XY7
             // Set vX to vY minus vX
             // set vF to 00 if a borrow occurs from beyond the highest bit
@@ -166,6 +163,13 @@ void cycle(Chip8 *c8, FILE *out) {
                     c8->v[0xF] = 0x01;
                 }
                 c8->v[X] = c8->v[Y] - c8->v[X];
+            }
+            // 8XYE
+            // Store the value of vY shifted one bit left in vX
+            // Set vF to the least most significant bit of vY prior to the shift
+            else if (N == 0x000E) {
+                c8->v[0xF] = ((c8->v[Y] & 0x8) >> 3); // store most significant bit in vF
+                c8->v[X] = (c8->v[Y] << 1);
             }
 
             break;
